f435ac1f911e30727be8c20553c19c4f9db913d5
Gabriel Lino <gabriel.lino@cittamobi.com.br>
2023-10-11 16:13:29 -0400

    a

---
+++ src/views/Dashboard.js
@@ -0,0 +1,201 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.3.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// react plugin used to create charts
+// reactstrap components
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+// core components
+
+function Dashboard() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col lg="3" md="6" sm="6">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="4" xs="5">
+                    <div className="icon-big text-center icon-warning">
+                      <i className="nc-icon nc-check-2 text-success" />
+                    </div>
+                  </Col>
+                  <Col md="8" xs="7">
+                    <div className="numbers">
+                      <p className="card-category">Status</p>
+                      <CardTitle tag="p">Aprovado</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="3" md="6" sm="6">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="4" xs="5">
+                    <div className="icon-big text-center icon-warning">
+                      <i className="nc-icon nc-money-coins text-success" />
+                    </div>
+                  </Col>
+                  <Col md="8" xs="7">
+                    <div className="numbers">
+                      <p className="card-category">Valor do Lance</p>
+                      <CardTitle tag="p">R$ 1.345,00</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="3" md="6" sm="6">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="4" xs="5">
+                    <div className="icon-big text-center icon-warning">
+                      <i className="nc-icon nc-paper text-danger" />
+                    </div>
+                  </Col>
+                  <Col md="8" xs="7">
+                    <div className="numbers">
+                      <p className="card-category">Contrato</p>
+                      <CardTitle tag="p">23</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="4" >
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="12" xs="12">
+                    <div className="numbers">
+                      <p className="card-category">Nome</p>
+                      <CardTitle tag="p">Gabriel Lino Topa Garcia</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="3">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="12" xs="12">
+                    <div className="numbers">
+                      <p className="card-category">CPF</p>
+                      <CardTitle tag="p">707.395.532-59</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="4" >
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="12" xs="12">
+                    <div className="numbers">
+                      <p className="card-category">Estado</p>
+                      <CardTitle tag="p">Mato Grosso do Sul</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="3">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="12" xs="12">
+                    <div className="numbers">
+                      <p className="card-category">Cidade</p>
+                      <CardTitle tag="p">Campo Grande</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg="4" >
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="12" xs="12">
+                    <div className="numbers">
+                      <p className="card-category">Endereço</p>
+                      <CardTitle tag="p">Rua Japão 221, Joquei Club</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col lg="3">
+            <Card className="card-stats">
+              <CardBody>
+                <Row>
+                  <Col md="12" xs="12">
+                    <div className="numbers">
+                      <p className="card-category">CEP</p>
+                      <CardTitle tag="p">79080450</CardTitle>
+                      <p />
+                    </div>
+                  </Col>
+                </Row>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
---
+++ src/views/User.js
@@ -0,0 +1,145 @@
+/*!
+
+=========================================================
+* Paper Dashboard React - v1.3.2
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+* Licensed under MIT (https://github.com/creativetimofficial/paper-dashboard-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+} from "reactstrap";
+
+function User() {
+  return (
+    <>
+      <div className="content">
+        <Row>
+          <Col md="8">
+            <Card className="card-user">
+              <CardHeader>
+                <CardTitle tag="h5">Análise de Proposta</CardTitle>
+              </CardHeader>
+              <CardBody>
+                <Form>
+                
+                  <Row>
+                    <Col className="pr-1" md="6">
+                      <FormGroup>
+                        <label>Nome</label>
+                        <Input
+                          defaultValue="Chet"
+                          placeholder="Company"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="6">
+                      <FormGroup>
+                        <label>Sobrenome</label>
+                        <Input
+                          defaultValue="Faker"
+                          placeholder="Last Name"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col md="12">
+                      <FormGroup>
+                        <label>Endereço</label>
+                        <Input
+                          defaultValue="Melbourne, Australia"
+                          placeholder="Home Address"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="pr-1" md="4">
+                      <FormGroup>
+                        <label>Cidade</label>
+                        <Input
+                          defaultValue="Melbourne"
+                          placeholder="City"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="px-1" md="4">
+                      <FormGroup>
+                        <label>Estado</label>
+                        <Input
+                          defaultValue="Australia"
+                          placeholder="Country"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="4">
+                      <FormGroup>
+                        <label>CEP</label>
+                        <Input placeholder="ZIP Code" type="number" />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="pr-1" md="6">
+                      <FormGroup>
+                        <label>Status</label>
+                        <Input
+                          defaultValue="Aprovado"
+                          placeholder="Company"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-1" md="6">
+                      <FormGroup>
+                        <label>Data Última Alteração</label>
+                        <Input
+                          defaultValue="Faker"
+                          placeholder="Last Name"
+                          type="text"
+                        />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  
+              
+                </Form>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+}
+
+export default User;
